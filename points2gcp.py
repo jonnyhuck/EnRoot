8e56f44d087a9a8bc7ff89725284a71b92c95bed
Jonny Huck <jonnyhuck@gmail.com>
2019-02-25 09:36:02 +0000

    initial commit

---
+++ points2gcp.py
@@ -0,0 +1,25 @@
+import csv
+
+# set csv path
+csv_path = "pointsfile.points"
+
+# init output string
+out = ""
+
+# open the file
+with open(csv_path, "rb") as file:
+
+	# read the file (binary mode)
+	reader = csv.reader(file)
+	
+	# skip the header line
+	next(reader, None)
+
+	# for each line
+	for row in reader:
+
+		# convert to required format and append to output string (-gcp pixel line easting northing elevation)
+		out += "-gcp " + " ".join([ str(round(float(row[2]),3)), str(abs(round(float(row[3]),3))), row[0], row[1]]) + " "
+
+# print output string
+print out
